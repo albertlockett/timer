825724be58c6652d9195b8da7b650339099cd4a8
albert lockett <albertlockett@alberts-MacBook-Pro.local>
2016-08-03 10:46:23 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var clear = require('clear');
+
+var time = 0;
+
+function displayTime() {
+  var now = new Date();
+
+  var year = now.getFullYear();
+  var month = now.getMonth();
+  var day = now.getDay();
+
+  var quittingTime = new Date(year, month, day, 17, 0, 0);
+
+  var diff = quittingTime - now;
+
+  var hours = Math.floor(diff / 1000 / 3600);
+  var minutes = Math.floor((diff / 1000) % 3600 / 60);
+  var seconds = Math.floor((diff / 1000) % 3600 % 60);
+  var ms = diff % 1000;
+
+  console.log("" + hours + "h " + minutes + "m " + seconds + "." + ms + "s");
+}
+
+function clearAndDisplay() {
+  clear();
+  displayTime();
+}
+
+setInterval(clearAndDisplay, 1000);
+
