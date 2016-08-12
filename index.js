82429e0921a7d2b7e62d07ed7551fb6ef5d60cda
albert lockett <albertlockett@alberts-MacBook-Pro.local>
2016-08-12 14:56:39 -0300

    install script added

--- index.js
+++ index.js
@@ -7,18 +7,19 @@ function displayTime() {
 
   var year = now.getFullYear();
   var month = now.getMonth();
-  var day = now.getDay();
+  var day = now.getDate();
 
   var quittingTime = new Date(year, month, day, 17, 0, 0);
-
+  
   var diff = quittingTime - now;
 
   var hours = Math.floor(diff / 1000 / 3600);
   var minutes = Math.floor((diff / 1000) % 3600 / 60);
   var seconds = Math.floor((diff / 1000) % 3600 % 60);
   var ms = diff % 1000;
+  var hundreths = Math.floor((diff % 1000) / 10);
 
-  console.log("" + hours + "h " + minutes + "m " + seconds + "." + ms + "s");
+  console.log("" + hours + "h " + minutes + "m " + seconds + "s");
 }
 
 function clearAndDisplay() {
@@ -26,5 +27,5 @@ function clearAndDisplay() {
   displayTime();
 }
 
-setInterval(clearAndDisplay, 1000);
-
+//setInterval(clearAndDisplay, 1000);
+displayTime();
